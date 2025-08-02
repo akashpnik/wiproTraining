d0dca19abdf3d9576f334b9402490f20dc9d036a
akashpnik <akashpnik@gmail.com>
2025-08-02 23:59:26 +0530

    Day10

---
+++ day10/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace CalculatorLib { 
+public interface ICalculator
+{
+    int Add(int a, int b);
+
+    int Subtract(int a, int b);
+
+    int Multiply(int a, int b);
+
+    int Divide(int a, int b);
+    //{
+    //    if (b == 0)
+    //        throw new DivideByZeroException("Cannot divide by zero.");
+    //    return a / b;
+    //}
+}
+public class Calculator : ICalculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    public int Divide(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException("Cannot divide by zero.");
+        return a / b;
+    }
+
+}
+}
---
+++ day10/CalculatorTests.cs
@@ -0,0 +1,58 @@
+﻿using CalculatorLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorLib.Tests
+{
+    public class  CalculatorTests
+    {
+        private Calculator calc;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsCorrectSum()
+        {
+            double result = calc.Add(3, 3);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Subtract_TwoNumbers_ReturnsCorrectDifference()
+        {
+            double result = calc.Subtract(10, 4);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Multiply_TwoNumbers_ReturnsCorrectProduct()
+        {
+            double result = calc.Multiply(2, 3);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void Divide_TwoNumbers_ReturnsCorrectQuotient()
+        {
+            double result = calc.Divide(10, 2);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Divide_ByZero_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() => calc.Divide(10, 0));
+        }
+
+        [TestMethod]
+        public void Add_WithZero_ReturnsSameNumber()
+        {
+            double result = calc.Add(0, 5);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
